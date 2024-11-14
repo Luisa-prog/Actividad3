1a98d3f66e90b00f5f825775cb0ad1f881f973e6
Luisa Leon Campos <luisaleon34563@gmail.com>
2024-11-14 19:28:46 +0000

    subiendo proyrcto

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+
+
+public class Main {
+  public static void main(String[] args) {
+
+
+    int PecesRojos = 284;
+    int PecesAzules =163;
+    int PecesTotal = PecesRojos + PecesAzules;
+
+    System.out.println("El total de peces es: " + PecesTotal);
+  }
+
+
+}
